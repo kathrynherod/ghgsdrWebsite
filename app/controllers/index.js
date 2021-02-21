02f69ce98b0159c1321293c54a83f9fd51fdaaf1
Kathryn Herod <kherod@idauto.net>
2021-02-20 22:20:36 -0600

    added filter by good with

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,38 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
+    filteredDogs: computed('goodWithBigDogs', 'goodWithSmallDogs', 'goodWithKids', 'goodWithCats', function() {
+        const {
+            adoptableDogs, goodWithBigDogs, goodWithSmallDogs, goodWithKids, goodWithCats,
+        } = this.getProperties(
+            'adoptableDogs', 'goodWithBigDogs', 'goodWithSmallDogs', 'goodWithKids', 'goodWithCats',
+        );
+
+        if (!goodWithBigDogs && !goodWithSmallDogs && !goodWithKids && !goodWithCats) {
+            return adoptableDogs;
+        }
+
+        let filteredDogs = adoptableDogs;
+
+        if (goodWithBigDogs) {
+            filteredDogs = filteredDogs.filter((dog) => dog.get('goodWithBigDogs') !== 'No');
+        }
+
+        if (goodWithSmallDogs) {
+            filteredDogs = filteredDogs.filter((dog) => dog.get('goodWithSmallDogs') !== 'No');
+        }
+
+        if (goodWithKids) {
+            filteredDogs = filteredDogs.filter((dog) => dog.get('goodWithKids') !== 'No');
+        }
+
+        if (goodWithCats) {
+            filteredDogs = filteredDogs.filter((dog) => dog.get('goodWithCats') !== 'No');
+        }
+
+        return filteredDogs;
+    }),
 
     actions: {
         launchModal(dogName, imageUrl) {
@@ -9,6 +41,22 @@ export default Controller.extend({
                 modalImageUrl: imageUrl,
                 modalTitle: dogName,
             });
+        },
+
+        toggleGoodWithBigDogs() {
+            this.toggleProperty('goodWithBigDogs');
+        },
+
+        toggleGoodWithSmallDogs() {
+            this.toggleProperty('goodWithSmallDogs');
+        },
+
+        toggleGoodWithKids() {
+            this.toggleProperty('goodWithKids');
+        },
+
+        toggleGoodWithCats() {
+            this.toggleProperty('goodWithCats');
         },
     },
 });
--- app/serializers/dog.js
+++ app/serializers/dog.js
@@ -23,21 +23,25 @@ export default DS.JSONSerializer.extend(EmbeddedRecordsMixin, {
 
             switch (attr.name) {
                 case 'Good with Big Dogs': {
+                    modifiedHash.goodWithBigDogs = attr.value;
                     icon = 'dog'
                     index = 0;
                     break;
                 }
                 case 'Good with Small Dogs': {
+                    modifiedHash.goodWithSmallDogs = attr.value;
                     icon = 'dog'
                     index = 1;
                     break;
                 }
                 case 'Good with Cats': {
+                    modifiedHash.goodWithCats = attr.value;
                     icon = 'cat'
                     index = 3;
                     break;
                 }
                 case 'Good with Kids': {
+                    modifiedHash.goodWithKids = attr.value;
                     icon = 'child'
                     index = 2;
                     break;
