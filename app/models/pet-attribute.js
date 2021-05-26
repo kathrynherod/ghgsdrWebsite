075fe81976c92d57a161f59a4544454488efe245
Kathryn Herod <kherod@idauto.net>
2021-05-25 21:17:40 -0500

    Deployed 824f042

--- app/models/pet-attribute.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-const { Model } = DS;
-
-export default Model.extend({
-    icon: DS.attr('string'),
-    index: DS.attr('number'),
-    name: DS.attr('string'),
-    value: DS.attr('string'),
-});
