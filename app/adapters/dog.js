a09b770c954f29fbec8935a1dca795c249b16427
Kathryn Herod <kherod@idauto.net>
2021-12-14 09:39:34 -0600

    update cors-anywhere version

--- app/adapters/dog.js
+++ app/adapters/dog.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import { computed } from '@ember/object';
 
 export default DS.JSONAPIAdapter.extend({
-    proxy: 'https://quiet-meadow-73531.herokuapp.com/',
+    proxy: 'https://hidden-bayou-45203.herokuapp.com/',
 
     host: computed(function() {
         const psUrl = 'https://petstablished.com/api/v2/public/pets?public_key=43eb4fe251a66bbdb3042ec3dcddb7c7';
